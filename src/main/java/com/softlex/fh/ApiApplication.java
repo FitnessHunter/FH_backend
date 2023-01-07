1c24439354086872ddfdef0d00a1763531b5b824
Dima Chabai <DChabai@ibagroup.eu>
2023-01-07 23:42:10 +0300

    config

--- src/main/java/com/softlex/fh/ApiApplication.java
+++ src/main/java/com/softlex/fh/ApiApplication.java
@@ -2,8 +2,10 @@ package com.softlex.fh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class ApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/softlex/fh/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.softlex.fh.config;
+
+
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+        .anyRequest()
+        .permitAll()
+        .antMatchers("/")
+        .permitAll()
+        .antMatchers("/**")
+        .authenticated()
+        .and()
+        .httpBasic();
+  }
+}
