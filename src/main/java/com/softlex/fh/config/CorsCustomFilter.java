c8bfcec89a317fea0f26c5d22b7ff7584c0d831a
DimaChabai <50172220+DimaChabai@users.noreply.github.com>
2023-01-24 15:24:54 +0300

    Cors
    Create policy

---
+++ src/main/java/com/softlex/fh/config/CorsCustomFilter.java
@@ -0,0 +1,65 @@
+package com.iba.lm.api.config;
+
+import java.io.IOException;
+
+import java.util.Set;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.google.common.net.HttpHeaders;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Slf4j
+public class CorsCustomFilter implements Filter {
+
+  private Set<String> allowedOrigins = Set.of("http://localhost:3000","http://localhost:8080","http://localhost:8090");
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        String origin = request.getHeader(HttpHeaders.ORIGIN);
+        log.debug("Received request from: " + origin);
+        if (allowedOrigins.contains(origin)) {
+        	response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods",
+        		"ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL, PATCH");
+        response.setHeader("Access-Control-Max-Age", "31536000");
+        response.setHeader("Access-Control-Allow-Headers",
+        		"Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        // not needed
+    }
+
+    @Override
+    public void destroy() {
+        //not needed
+    }
+
+}
